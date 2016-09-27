b39cb06d31494a33f6181eaad6a4d96e6614888d
holla-dolla <Alexyaroschuck@yandex.ua>
2016-09-27 22:07:58 +0300

    started Automapper  implementation

---
+++ SKAPI/SKAPI.BL.Objects/Common/AutoMapperConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SKAPI.BL.Objects.Schedule.Responce;
+using SKAPI.BL.Objects.Schedule;
+
+namespace SKAPI.BL.Objects.Common
+{
+    public static class AutoMapperConfig    
+    {
+        public static void RegisterMaps()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<TimeLine, ScheduleResponce>()
+                .ForMember(dest => dest.Group,
+                 opt => opt.MapFrom(src => src.Data.Group))
+                 .ForMember(dest=> dest.Weeks,
+                 opt => opt.ResolveUsing<WeekResolver>());
+
+                config.CreateMap<Week, WeekResponce>()
+                .ForMember(dest => dest.Number,
+                opt => opt.MapFrom(src => src.Number));
+            });
+        }
+    }
+}
---
+++ SKAPI/SKAPI.BL.Objects/Common/WeekResolver.cs
@@ -0,0 +1,28 @@
+﻿using SKAPI.BL.Objects.Schedule;
+using SKAPI.BL.Objects.Schedule.Responce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace SKAPI.BL.Objects.Common
+{
+    public class WeekResolver : IValueResolver<TimeLine, ScheduleResponce, List<WeekResponce>>
+    {
+        public List<WeekResponce> Resolve(TimeLine source, ScheduleResponce destination, List<WeekResponce> destMember, ResolutionContext context)
+        {
+            AutoMapperConfig.RegisterMaps();
+
+            List<WeekResponce> result = new List<WeekResponce>();
+
+            WeekResponce week = new WeekResponce();
+
+            destMember.Add(Mapper.Map<Week, WeekResponce>(source.Data.ListWeeks.FirtsWeek));
+            destMember.Add(Mapper.Map<Week, WeekResponce>(source.Data.ListWeeks.SecondWeek));
+
+            return destMember;
+        }
+    }
+}
